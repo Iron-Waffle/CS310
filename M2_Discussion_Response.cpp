2c13960289967616b8103e8e7fea618ad51f6d4c
Boris Davis <bodavis@my365.bellevue.edu>
2024-09-06 22:58:50 -0500

    Added M2_Discussion_Response.cpp

---
+++ M2_Discussion_Response.cpp
@@ -0,0 +1,22 @@
+// M2_Discussion_response
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1, num2;
+
+    std::cout << "Enter the first number: ";
+    std::cin >> num1;
+
+    std::cout << "Enter the second number: ";
+    std::cin >> num2;
+
+    int sum = num1 + num2; // Corrected num2 and adjusted it to num2;
+
+        std::cout << "The sum is: " << sum << std::endl; // Corrected std::end and adjusted it to std::endl;
+
+    return 0;
+}
