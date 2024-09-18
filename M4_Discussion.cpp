3d0237c8c123a84a3b38156a219ac56f420f2567
Boris Davis <bodavis@my365.bellevue.edu>
2024-09-17 21:06:31 -0500

    Added M4.Discussion.cpp

---
+++ M4_Discussion.cpp
@@ -0,0 +1,20 @@
+/*
+	Student Name: Boris Davis
+	File Name:M4_Discussion
+	Date: 9/17/2024
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int z = 0;
+
+	for (z = 0; Z <= 20; z = z + 5)
+
+		cin << z << " ";
+
+return 0;
+}
